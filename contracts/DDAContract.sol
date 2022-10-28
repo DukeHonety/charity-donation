77c2e57664041c65edc122c435b0b7d7d60060f9
sacred-danny <sacred.danny.jin@gmail.com>
2022-10-28 10:19:36 +0900

    feat: CreateCharity and CreateFundRaiser

--- contracts/DDAContract.sol
+++ contracts/DDAContract.sol
@@ -2,7 +2,7 @@
 pragma solidity ^0.8.6;
 import "hardhat/console.sol";
 
-
+import "@chainlink/contracts/src/v0.8/interfaces/AggregatorV3Interface.sol";
 import "@openzeppelin/contracts/token/ERC20/IERC20.sol";
 import "@openzeppelin/contracts/utils/structs/EnumerableSet.sol";
 import "@openzeppelin/contracts/access/AccessControl.sol";
@@ -20,20 +20,30 @@ contract DDAContract is AccessControl {
     }
     struct CharityType {
         address _address;
-        string title;
+        string name;
+        string vip;
+        string website;
+        string email;
         string country;
-        string location;
-        string _type;
+        string summary;
+        string detail;
+        string photo;
+        uint256 fund;
     }
     struct FundRaiserType {
         address _address;
-        uint256 goal;
-        uint256 fund;
+        string title;
         string name;
+        string email;
         string country;
         string location;
+        string summary;
         string story;
         string _type;
+        uint256 goal;
+        uint256 fund;
+        string photo;
+        uint256 timestamp;
     }
     mapping(address => DonaterType) public donaters;
     mapping(address => CharityType) public charities;
@@ -41,15 +51,21 @@ contract DDAContract is AccessControl {
     event Donate(
         address indexed _from,
         address indexed _to,
+        address indexed currency,
         uint256 _amount,
+        uint256 price,
         uint256 timestamp
     );
     event CreateCharity(
         address indexed _address,
-        string title,
-        string country,
-        string location,
-        string indexed _type,
+        string name,
+        string vip,
+        string website,
+        string email,
+        string indexed country,
+        string summary,
+        string detail,
+        string photo,
         uint256 timestamp
     );
     event RemoveCharity(
@@ -58,13 +74,16 @@ contract DDAContract is AccessControl {
     );
     event CreateFundRaiser(
         address indexed _address,
-        uint256 goal,
-        uint256 fund,
+        string title,
         string name,
+        string email,
         string country,
         string location,
+        string summary,
         string story,
         string indexed _type,
+        uint256 goal,
+        string photo,
         uint256 timestamp
     );
     event RemoveFundRaiser(
@@ -97,20 +116,21 @@ contract DDAContract is AccessControl {
     /**
      * Donation function
      */
-    function donate(address _to, address _currency, uint256 _amount) external {
+    function donate(address _to, address _currency, uint256 _amount, uint256 price) external {
         IERC20 currency = IERC20(_currency);
         require ( _amount > 0, "Deposit amount error");
-        require (currency.balanceOf(msg.sender) < _amount, "Not enough tokens!");
+        require (currency.balanceOf(msg.sender) > _amount, "Not enough tokens!");
         require (donaters[msg.sender]._address != address(0) , "Donater's address isn't registered!");
-        require (fundRaisers[_to]._address != address(0) , "FundRaiser's address isn't registered!");
+        require (fundRaisers[_to]._address != address(0) || charities[_to]._address != address(0), "FundRaiser's address isn't registered!");
         uint256 ratio = 10;
-        if (_amount > 250000 ether) {
+        uint256 usdAmount = _amount * price / 1 ether;
+        if (usdAmount > 250000 ether) {
             ratio = 1;
-        } else if (_amount > 100000 ether) {
+        } else if (usdAmount > 100000 ether) {
             ratio = 3;
-        } else if (_amount > 50000 ether) {
+        } else if (usdAmount > 50000 ether) {
             ratio = 5;
-        } else if (_amount > 10000 ether) {
+        } else if (usdAmount > 10000 ether) {
             ratio = 7;
         } else {
             ratio = 10;
@@ -118,12 +138,13 @@ contract DDAContract is AccessControl {
 
         uint256 _transferAmount = _amount * (1000 - ratio) / 1000;
         uint256 _buyAmount = _amount * ratio / 1000;
-
-        // fundRaisers[_to].fund += _transferAmount;
+        if (fundRaisers[_to]._address != address(0))
+            fundRaisers[_to].fund = fundRaisers[_to].fund + _transferAmount*price / 1 ether;
+        if (charities[_to]._address != address(0))
+            charities[_to].fund = fundRaisers[_to].fund + _transferAmount*price / 1 ether;
         currency.approve(address(this), _amount);
-        currency.transferFrom(msg.sender, _to, _transferAmount);
-        emit Donate(msg.sender, _to, _transferAmount, block.timestamp);
-
+        // currency.transferFrom(msg.sender, _to, _transferAmount);
+        emit Donate(msg.sender, _to, _currency, _transferAmount, price, block.timestamp);
     }
 
     function createDonater(string memory name) external hasExistAddress{
@@ -143,18 +164,23 @@ contract DDAContract is AccessControl {
         donaters[msg.sender]._address = address(0);
         emit RemoveDonater(msg.sender, block.timestamp);
     }
-    function createCharity(string memory title, string memory country, string memory location, string memory _type) external hasExistAddress {
-        require(bytes(title).length > 0 && bytes(country).length > 0 && bytes(location).length > 0 && bytes(_type).length > 0, 'There is empty string passed as parameter');
+    function createCharity(string memory name, string memory vip, string memory website, string memory email, string memory country, string memory summary, string memory detail, string memory photo) external hasExistAddress {
+        require(bytes(name).length > 0 && bytes(vip).length > 0 && bytes(website).length > 0 && bytes(email).length > 0 && bytes(country).length > 0 && bytes(summary).length > 0 && bytes(detail).length > 0 && bytes(photo).length > 0, 'There is empty string passed as parameter');
 
         addressIdxs.add(msg.sender);
         charities[msg.sender] = CharityType({
             _address: msg.sender,
-            title: title,
+            name: name,
+            vip: vip,
+            website: website,
+            email: email,
             country: country,
-            location: location,
-            _type: _type
+            summary: summary,
+            detail: detail,
+            photo: photo,
+            fund: 0
         });
-        emit CreateCharity(msg.sender, title, country, location, _type, block.timestamp);
+        emit CreateCharity(msg.sender, name, vip, website, email, country, summary, detail, photo, block.timestamp);
     }
     function removeCharity() public {
         require(charities[msg.sender]._address != address(0), 'This address is not exist');
@@ -162,22 +188,28 @@ contract DDAContract is AccessControl {
         charities[msg.sender]._address = address(0);
         emit RemoveCharity(msg.sender, block.timestamp);
     }
-    function createFundRaiser(uint256 goal, string memory name, string memory country, string memory location, string memory story, string memory _type) external hasExistAddress {
+    
+    function createFundRaiser(string memory title, string memory name, string memory email, string memory country, string memory location, string memory summary, string memory story, string memory _type, uint256 goal, string memory photo) external hasExistAddress {
         require(goal >= 1 ether, 'Raise money could be at least $1');
-        require(bytes(name).length > 0 && bytes(country).length > 0  && bytes(location).length > 0 && bytes(story).length > 0 && bytes(_type).length > 0, 'There is empty string passed as parameter');
+        require(bytes(title).length > 0 && bytes(name).length > 0 && bytes(email).length > 0 && bytes(country).length > 0  && bytes(location).length > 0 && bytes(summary).length > 0 && bytes(story).length > 0 && bytes(_type).length > 0 && bytes(photo).length > 0, 'There is empty string passed as parameter');
 
         addressIdxs.add(msg.sender);
         fundRaisers[msg.sender] = FundRaiserType({
             _address: msg.sender,
-            goal: goal,
-            fund: 0,
+            title: title,
             name: name,
+            email: email,
             country: country,
             location: location,
+            summary: summary,
             story: story,
-            _type: _type
+            _type: _type,
+            goal: goal,
+            fund: 0,
+            photo: photo,
+            timestamp: block.timestamp
         });
-        emit CreateFundRaiser(msg.sender, goal, 0, name, country, location, story, _type, block.timestamp);
+        emit CreateFundRaiser(msg.sender, title, name, email, country, location, summary, story, _type, goal, photo, block.timestamp);
     }
     function removeFundRaiser() public {
         require(fundRaisers[msg.sender]._address != address(0), 'This address is not exist');
--- test/test.ts
+++ test/test.ts
@@ -1,29 +1,34 @@
 import { SignerWithAddress } from "@nomiclabs/hardhat-ethers/signers";
 import { Contract } from "ethers";
 import { ethers } from "hardhat";
+import env from 'dotenv'
+
 describe("DDAContract Test network", () => {
   let Token;
   let ddaContract: Contract,
     tusdtToken: Contract,
     deployer: SignerWithAddress,
-    admin: SignerWithAddress;
+    donater1: SignerWithAddress,
+    donater2: SignerWithAddress,
+    fundRaiser1: SignerWithAddress,
+    fundRaiser2: SignerWithAddress,
+    charity1: SignerWithAddress;
+
   describe("Deploying", () => {
     it("Deployed test currencies", async () => {
-      [deployer] = await ethers.getSigners(); //??
+      [deployer, donater1, donater2, fundRaiser1, fundRaiser2, charity1] = await ethers.getSigners(); //??
       console.log("deployer: ", deployer.address);
-      // console.log("admin: ", admin.address);
       Token = await ethers.getContractFactory("TUSDT");
       tusdtToken = await Token.deploy(deployer.address);
       await tusdtToken.deployed();
-      console.log("TUSDT address: ", tusdtToken.address);
+      console.log("[TUSDT address] : ", tusdtToken.address);
       console.log(
         "tusdtToken verify: ",
         `npx hardhat verify --contract "contracts/TUSDT.sol:TUSDT" --network goerli ${tusdtToken.address} ${deployer.address}`
       );
-      let tx = await tusdtToken
-        .connect(deployer)
-        .mint(deployer.address, "10000000000000000000000000000");
-      await tx.wait();
+      await tusdtToken.connect(deployer).mint(deployer.address, "10000000000000000000000000000"); // 10000000000 ether
+      await tusdtToken.connect(deployer).mint(donater1.address, "10000000000000000000000000000");
+      await tusdtToken.connect(deployer).mint(donater2.address, "10000000000000000000000000000");
     });
 
     it("Deployed DDAContract", async () => {
@@ -39,38 +44,91 @@ describe("DDAContract Test network", () => {
         `npx hardhat verify --contract "contracts/DDAContract.sol:DDAContract" --network goerli ${ddaContract.address} ${deployer.address}`
       );
     });
+
   });
   describe("Doing Registers", () => {
-    ddaContract.connect(env.)
-    ddaContract.method.createDonater();
+    it("Create Donater", async () => {
+      console.log('////////////////Create two donaters with anme First and Second////////////////////////');
+      await ddaContract.connect(donater1).createDonater('First');
+      await ddaContract.connect(donater2).createDonater('Second');
+      // await ddaContract.connect(donater1).createDonater('First');
+      console.log(await ddaContract.donaters(donater1.address));
+      console.log(await ddaContract.donaters(donater2.address));
+    });
+
+    it("Create Charity", async () => {
+      console.log('////////////////Create on charity "healthcare charity"////////////////////////');
+      /**
+       * @param string memory name
+       * @param string memory vip
+       * @param string memory website
+       * @param string memory email
+       * @param string memory country
+       * @param string memory summary
+       * @param string memory detail
+       * @param string memory photo
+       */
+      await ddaContract.connect(charity1)
+            .createCharity('Healthcare Charity',
+            'cnc-12332322',
+            'http://healthcare.com',
+            'HelpBrian@gmail.com',
+            'US',
+            'On October 24, our godson Brian was in health class at Central Visual Performing Arts (VPA) high school in St. Louis, Missouri, when his school went on lockdown',
+            'On October 24, our godson Brian was in health class at Central Visual Performing Arts (VPA) high school in St. Louis, Missouri, when his school went on lockdown. Minutes later, a shooter entered his classroom, killing his teacher and wounding Brian and several classmates.\nBrian was shot through his hands and a bullet lodged in his jaw, missing a major artery by centimeters. After being shot, Brian and several classmates miraculously escaped through a second-story window, jumping from the building and over a fence to get to safety. Brian was treated at St. Louis Children\'s Hospital by an incredible medical team and was discharged this evening to begin his longer-term recovery and healing at home.',
+            'file:///');
+      console.log(await ddaContract.charities(charity1.address));
+    });
+
+    // it("Created fundRaiser", async() => {
+    //   console.log('//////////////////////////////Create fundRaiser');
+    //   /**
+    //    * 
+    //    * @param string memory title
+    //    * @param string string memory name
+    //    * @param string string memory email
+    //    * @param string string memory country
+    //    * @param string string memory location
+    //    * @param string string memory summary
+    //    * @param string string memory story
+    //    * @param string string memory _type
+    //    * @param string uint256 goal
+    //    * @param string string memory photo
+    //    */
+    //   await ddaContract.connect(fundRaiser1)
+    //         .createFundRaiser('Help Brian Heal After Surviving a School Shooting',
+    //         'Brian',
+    //         'HelpBrian@gmail.com',
+    //         'US',
+    //         'San Diego',
+    //         'On October 24, our godson Brian was in health class at Central Visual Performing Arts (VPA) high school in St. Louis, Missouri, when his school went on lockdown',
+    //         'On October 24, our godson Brian was in health class at Central Visual Performing Arts (VPA) high school in St. Louis, Missouri, when his school went on lockdown. Minutes later, a shooter entered his classroom, killing his teacher and wounding Brian and several classmates.\nBrian was shot through his hands and a bullet lodged in his jaw, missing a major artery by centimeters. After being shot, Brian and several classmates miraculously escaped through a second-story window, jumping from the building and over a fence to get to safety. Brian was treated at St. Louis Children\'s Hospital by an incredible medical team and was discharged this evening to begin his longer-term recovery and healing at home.',
+    //         'Accident',
+    //         '100000000000000000000000',
+    //         'file:///');
+    //   console.log(await ddaContract.fundRaisers(fundRaiser1.address));
+    // });
+
+    it("Transfer Donation", async() => {
+      console.log('//////////////////////////////Do Donation');
+      /**
+       * @param string memory title
+       */
+      console.log('[Donater1 currency:]', (await tusdtToken.balanceOf(donater1.address)).toString());
+      console.log('[Donater1 transfer donation 2 times: 100 TUSDT and 60000 TUSDT]');
+      const tUsdtPrice = '1200000000000000000';
+      console.log('[TUSDT price] : $1.2');
+
+      // await tusdtToken.connect(donater1).approve(ddaContract.address, '10000000000000000000000');
+      let val = await ddaContract.connect(donater1).donate(charity1.address, tusdtToken.address, ethers.utils.parseUnits('100', 'ethers'), tUsdtPrice); // 10000 ether
+      let charityFund = (await ddaContract.charities(charity1.address))['fund'];
+      console.log('[Charity fund]', await ethers.utils.formatEther(charityFund));
+
+      // await ddaContract.connect(donater1).donate(fundRaiser1.address, tusdtToken.address, '60000000000000000000000'); // 10000 ether
+      // console.log('[FundRaiser fund]',(await ddaContract.fundRaisers(fundRaiser1.address))['fund'].toString());
+
+      console.log('[Donater1 currency:]', (await tusdtToken.balanceOf(donater1.address)).toString());
+    });
   });
 
-  // describe("Deploy", () => {
-  //   it("Should deploy the contracts", async () => {
-  //     [deployer] = await ethers.getSigners();  //??
-  //     console.log("deployer: ", deployer.address);
-  //     // console.log("admin: ", admin.address);
-  //     Token = await ethers.getContractFactory("TUSDT");
-  //     tusdtToken = await Token.deploy(deployer.address);
-  //     await tusdtToken.deployed();
-  //     console.log("tusdtToken address: ", tusdtToken.address);
-  //     console.log(
-  //       "tusdtToken verify: ",
-  //       `npx hardhat verify --contract "contracts/TUSDT.sol:TUSDT" --network bscTestnet ${tusdtToken.address} ${deployer.address}`
-  //     );
-  //   });
-  // });
-
-  // describe("TUSDT Token Mint", () => {
-  //   it("Should mint tokens between accounts", async () => {
-  //     let tx = await tusdtToken
-  //       .connect(deployer)
-  //       .mint(deployer.address, "1000000000000000000000000");
-  //     await tx.wait();
-  //     // tx = await tusdtToken
-  //     //   .connect(admin)
-  //     //   .mint(admin.address, "10000000000000000000000000000");
-  //     // await tx.wait();
-  //   });
-  // });
 });
