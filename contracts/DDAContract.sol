96e3feba5e1a4425b23e7a2c9244fb37230417f1
sacred-danny <sacred.danny.jin@gmail.com>
2022-10-30 02:09:33 +0900

    recon: Set variable on constructor

--- contracts/DDAContract.sol
+++ contracts/DDAContract.sol
@@ -1,20 +1,20 @@
 // SPDX-License-Identifier: MIT
 pragma solidity 0.8.14;
 import "hardhat/console.sol";
-
-import "@chainlink/contracts/src/v0.8/interfaces/AggregatorV3Interface.sol";
 import "@openzeppelin/contracts/token/ERC20/IERC20.sol";
-import "@openzeppelin/contracts/utils/structs/EnumerableSet.sol";
 import "@openzeppelin/contracts/access/AccessControl.sol";
-import '@uniswap/v2-periphery/contracts/interfaces/IUniswapV2Router02.sol';
 import '@thesolidchain/pancake-swap-periphery/contracts/interfaces/IPancakeRouter02.sol';
 contract DDAContract is AccessControl {
-    using EnumerableSet for EnumerableSet.AddressSet;
-    EnumerableSet.AddressSet private addressIdxs;
-    enum CharityType{ CHARITY, FUNDRAISER }
-    address public SWAP_ROUTER_ADDRESS = 0xD99D1c33F9fC3444f8101754aBC46c52416550D1;
-    address public WETH = 0xae13d989daC2f0dEbFf460aC112a837C89BAa7cd;
-    address public okapiToken = 0xBE301038121CbbA399843d5B88799CAdAb027Fe6;
+    
+    enum CharityType {
+        CHARITY,
+        FUNDRAISER
+    }
+
+    address public SWAP_ROUTER_ADDRESS;
+    address public WETH_ADDRESS;
+    address public USDT_ADDRESS;
+    address public OKAPI_ADDRESS;
 
     bytes32 private ADMIN_ROLE;
     struct Catalog {
@@ -30,63 +30,82 @@ contract DDAContract is AccessControl {
         string location; // fundRaiser
     }
     struct CharityStruct {
-        address _address;
-        CharityType _type;
+        address wallet_address;
+        CharityType charityType;
         uint256 fund; // fundRaiser
         Catalog catalog;
     }
     
     CharityStruct[] public charities;
-    uint public totalCharity;
-    mapping(address => bool) private addressAry;
+    mapping(address => bool) private isExistAddress;
+
     modifier hasAdminRole() {
         require(hasRole(ADMIN_ROLE, msg.sender), "Caller is not an admin");
         _;
     }
-    event Donate(address indexed _from, address indexed _to, address indexed _currency, uint256 amount, uint256 timestamp);
+
+    event Donate(
+        address indexed _from,
+        address indexed _to,
+        address indexed _currency,
+        uint256 amount,
+        uint256 timestamp
+    );
+
     event CreateCharity(
-        address _address,
-        CharityType _type,
-        Catalog _catalog,
-        uint256 _fund,
+        address wallet_address,
+        CharityType charityType,
+        Catalog catalog,
+        uint256 fund,
         uint256 timestamp
     );
+
     event RemoveCharity(
-        address indexed _address,
+        address indexed wallet_address,
         uint256 timestamp
     );
-    constructor(address _admin) {
+    constructor(address _admin, address _swapRouter, address _weth, address _usdt, address _okapi) {
         _setupRole(ADMIN_ROLE, _admin);
-        totalCharity = 0;
+        SWAP_ROUTER_ADDRESS = _swapRouter;
+        WETH_ADDRESS = _weth;
+        USDT_ADDRESS = _usdt;
+        OKAPI_ADDRESS = _okapi;
     }
     function donate(uint256 _to, address _currency, uint256 _amount) external {
         IERC20 currency = IERC20(_currency);
+        
         require (_amount > 1 ether, "Deposit amount error");
         require (currency.balanceOf(msg.sender) > _amount, "Not enough tokens!");
-        require (addressAry[charities[_to]._address], "FundRaiser's address isn't registered!");
-        require (msg.sender != charities[_to]._address, 'You can not send yourself');
+        require (isExistAddress[charities[_to].wallet_address], "FundRaiser's address isn't registered!");
+        require (msg.sender != charities[_to].wallet_address, 'You can not send yourself');
+
+        uint256 price = 1 ether;
+        if (_currency == USDT_ADDRESS)
+            price = 2 ether;
+        uint256 usdtAmount = _amount * price / 1 ether;
         uint256 ratio = 10;
-        if (_amount >= 250000 ether) {
+
+        if (usdtAmount >= 250000 ether) {
             ratio = 1;
-        } else if (_amount >= 100000 ether) {
+        } else if (usdtAmount >= 100000 ether) {
             ratio = 3;
-        } else if (_amount >= 50000 ether) {
+        } else if (usdtAmount >= 50000 ether) {
             ratio = 5;
-        } else if (_amount >= 10000 ether) {
+        } else if (usdtAmount >= 10000 ether) {
             ratio = 7;
         } else {
             ratio = 10;
         }
 
-        uint256 _transferAmount = _amount * (100 - ratio) / 100;
-        uint256 _buyAmount = _amount * ratio / 100;
-        charities[_to].fund = charities[_to].fund + _transferAmount;
-        currency.transferFrom(msg.sender, charities[_to]._address, _transferAmount);
-        swap(_currency, okapiToken, _buyAmount, 0, msg.sender);
-        emit Donate(msg.sender, charities[_to]._address, _currency, _transferAmount, block.timestamp);
+        uint256 transferAmount = _amount * (100 - ratio) / 100;
+        uint256 buyAmount = _amount * ratio / 100;
+        charities[_to].fund = charities[_to].fund + transferAmount * price / 1 ether;
+        currency.transferFrom(msg.sender, charities[_to].wallet_address, transferAmount);
+        // swap(_currency, OKAPI_ADDRESS, buyAmount, 0, msg.sender);
+        emit Donate(msg.sender, charities[_to].wallet_address, _currency, transferAmount, block.timestamp);
     }
     function createCharity(CharityType _type, Catalog calldata _catalog) external {
-        require(!addressAry[msg.sender], 'This address is already exist');
+        require(!isExistAddress[msg.sender], 'This address is already exist');
         require( bytes(_catalog.email).length > 0 &&
                  bytes(_catalog.country).length > 0 &&
                  bytes(_catalog.summary).length > 0 &&
@@ -94,36 +113,22 @@ contract DDAContract is AccessControl {
                  bytes(_catalog.photo).length > 0,
                  'There is empty string passed as parameter');
 
-        addressIdxs.add(msg.sender);
-        addressAry[msg.sender] = true;
         charities.push(CharityStruct({
-            _address: msg.sender,
-            _type: _type,
+            wallet_address: msg.sender,
+            charityType: _type,
             catalog: _catalog,
             fund:0
         }));
-        totalCharity += 1;
+        isExistAddress[msg.sender] = true;
         emit CreateCharity(msg.sender, _type, _catalog, 0,  block.timestamp);
     }
     function removeCharity(uint index) public hasAdminRole{
-        address userAddress = charities[index]._address;
-        addressIdxs.remove(userAddress);
-        addressAry[userAddress] = false;
+        address userAddress = charities[index].wallet_address;
+        isExistAddress[userAddress] = false;
         delete charities[index];
-        totalCharity -= 1;
         emit RemoveCharity(userAddress, block.timestamp);
     }
-
-    // buy okapi on uniswap
-    function updateOkapi(address _okapiAddress) external hasAdminRole{
-        okapiToken = _okapiAddress;
-    }
-    function updateRouter(address _swapRouter) external hasAdminRole{
-        SWAP_ROUTER_ADDRESS = _swapRouter;
-    }
-    function updateWETH(address _wethAddress) external hasAdminRole{
-        WETH = _wethAddress;
-    }
+    
     function swap(address _tokenIn, address _tokenOut, uint256 _amountIn, uint256 _amountOutMin, address _to) public {
         IERC20(_tokenIn).transferFrom(msg.sender, address(this), _amountIn);
         
@@ -135,4 +140,15 @@ contract DDAContract is AccessControl {
         path[1] = _tokenOut;
         IPancakeRouter02(SWAP_ROUTER_ADDRESS).swapExactTokensForTokens(_amountIn, _amountOutMin, path, _to, block.timestamp + 60);
     }
+
+    // function getTokenPrice(address pairAddress, uint amount) public view returns(uint)
+    // {
+    //     IUniswapV2Pair pair = IUniswapV2Pair(pairAddress);
+    //     IERC20 token1 = IERC20(pair.token1);
+    //     (uint Res0, uint Res1,) = pair.getReserves();
+
+    //     // decimals
+    //     uint res0 = Res0*(10**token1.decimals());
+    //     return((amount*res0)/Res1); // return amount of token0 needed to buy token1
+    // }
 }
--- test/test.ts
+++ test/test.ts
@@ -21,36 +21,37 @@ describe("DDAContract Test network", () => {
       [deployer, donater1, donater2, fundRaiser1, fundRaiser2, charity1] = await ethers.getSigners();
       console.log("deployer: ", deployer.address);
     });
-    // it("Deployed test usdt token", async () => {
-    //   const TUSDT = await ethers.getContractFactory("TUSDT");
-    //   tUsdtToken = await TUSDT.deploy(deployer.address);
-    //   await tUsdtToken.deployed();
-    //   console.log("[TUSDT address] : ", tUsdtToken.address);
-    //   console.log(
-    //     "tusdtToken verify: ",
-    //     `npx hardhat verify --contract "contracts/TUSDT.sol:TUSDT" --network goerli ${tUsdtToken.address} ${deployer.address}`
-    //   );
-    //   await tUsdtToken.connect(deployer).mint(deployer.address, Web3.utils.toWei('1000000000000', 'ether')); // 10000000000 ether
-    //   await tUsdtToken.connect(deployer).mint(donater1.address, Web3.utils.toWei('10000000000', 'ether'));
-    //   await tUsdtToken.connect(deployer).mint(donater2.address, Web3.utils.toWei('10000000000', 'ether'));
-    // });
-    // it("Deployed test okapi token", async () => {
-    //   const OKAPI = await ethers.getContractFactory("TOKAPI");
-    //   okapiToken = await OKAPI.deploy(deployer.address);
-    //   await okapiToken.deployed();
-    //   console.log("[OKAPI address] : ", okapiToken.address);
-    //   console.log(
-    //     "okapiToken verify: ",
-    //     `npx hardhat verify --contract "contracts/TOKAPI.sol:TOKAPI" --network bscTestnet ${okapiToken.address} ${deployer.address}`
-    //   );
-    //   await okapiToken.connect(deployer).mint(deployer.address, Web3.utils.toWei('1000000000000', 'ether'));
-    // });
+    it("Deployed test usdt token", async () => {
+      const TUSDT = await ethers.getContractFactory("TUSDT");
+      tUsdtToken = await TUSDT.deploy(deployer.address);
+      await tUsdtToken.deployed();
+      
+      console.log("[TUSDT address] : ", tUsdtToken.address);
+      console.log(
+        "tusdtToken verify: ",
+        `npx hardhat verify --contract "contracts/TUSDT.sol:TUSDT" --network testnet ${tUsdtToken.address} ${deployer.address}`
+      );
+      await tUsdtToken.connect(deployer).mint(deployer.address, Web3.utils.toWei('1000000000000', 'ether')); // 10000000000 ether
+      await tUsdtToken.connect(deployer).mint(donater1.address, Web3.utils.toWei('10000000000', 'ether'));
+      await tUsdtToken.connect(deployer).mint(donater2.address, Web3.utils.toWei('10000000000', 'ether'));
+    });
+    it("Deployed test okapi token", async () => {
+      const OKAPI = await ethers.getContractFactory("TOKAPI");
+      okapiToken = await OKAPI.deploy(deployer.address);
+      await okapiToken.deployed();
+      console.log("[OKAPI address] : ", okapiToken.address);
+      console.log(
+        "okapiToken verify: ",
+        `npx hardhat verify --contract "contracts/TOKAPI.sol:TOKAPI" --network bscTestnet ${okapiToken.address} ${deployer.address}`
+      );
+      await okapiToken.connect(deployer).mint(deployer.address, Web3.utils.toWei('1000000000000', 'ether'));
+    });
     it("Deployed DDAContract", async () => {
       [deployer] = await ethers.getSigners(); //??
       console.log("deployer: ", deployer.address);
       // console.log("admin: ", admin.address);
       const DDAContract = await ethers.getContractFactory("DDAContract");
-      ddaContract = await DDAContract.deploy(deployer.address);
+      ddaContract = await DDAContract.deploy(deployer.address, deployer.address, deployer.address, tUsdtToken.address, okapiToken.address);
       await ddaContract.deployed();
       console.log("DDAContract address: ", ddaContract.address);
       console.log(
@@ -60,51 +61,46 @@ describe("DDAContract Test network", () => {
     });
 
   });
-  // describe("Doing Registers", () => {
-  //   it("Create Charity", async () => {
-  //     const information = {
-  //       vip: '',
-  //       website: '',
-  //       name: 'Brian',
-  //       email: 'Brian@gmail.com',
-  //       country: 'US',
-  //       summary: 'Help Brian Heal After Surviving a School Shooting',
-  //       detail: 'On October 24, our godson Brian was in health class at Central Visual Performing Arts (VPA) high school in St. Louis, Missouri, when his school went on lockdown. Minutes later, a shooter entered his classroom, killing his teacher and wounding Brian and several classmates',
-  //       photo: 'http://ipfs',
-  //       title: 'Help Brian Heal After Surviving a School Shooting',
-  //       location: 'Washington'
-  //     };
-  //     await ddaContract.connect(charity1).createCharity('0', information);
-  //     await ddaContract.connect(fundRaiser1).createCharity('1', information);
-  //     console.log(await ddaContract.connect(charity1).charities(0));
-  //   });
+  describe("Doing Registers", () => {
+    it("Create Charity", async () => {
+      const information = {
+        vip: '',
+        website: '',
+        name: 'Brian',
+        email: 'Brian@gmail.com',
+        country: 'US',
+        summary: 'Help Brian Heal After Surviving a School Shooting',
+        detail: 'On October 24, our godson Brian was in health class at Central Visual Performing Arts (VPA) high school in St. Louis, Missouri, when his school went on lockdown. Minutes later, a shooter entered his classroom, killing his teacher and wounding Brian and several classmates',
+        photo: 'http://ipfs',
+        title: 'Help Brian Heal After Surviving a School Shooting',
+        location: 'Washington'
+      };
+      await ddaContract.connect(charity1).createCharity('0', information);
+      await ddaContract.connect(fundRaiser1).createCharity('1', information);
+      console.log(await ddaContract.connect(charity1).charities(0));
+    });
 
-  //   it("Transfer Donation", async() => {
-  //     console.log('//////////////////////////////Do Donation//////////////////////////////');
-  //     let weiCurrency = await tUsdtToken.balanceOf(donater1.address);
-  //     console.log('[Donater1 currency (TUSDT):]', ethers.utils.formatEther(weiCurrency));
-  //     const donation1 = '100';
-  //     const donation2 = '60000';
-  //     console.log('[Transfer donation 2 times: ' + donation1 + ' TUSDT and ' + donation2 + ' TUSDT]');
+    it("Transfer Donation", async() => {
+      console.log('//////////////////////////////Do Donation//////////////////////////////');
+      let weiCurrency = await tUsdtToken.balanceOf(donater1.address);
+      console.log('[Donater1 currency (TUSDT):]', ethers.utils.formatEther(weiCurrency));
+      const donation1 = '100';
+      const donation2 = '60000';
+      console.log('[Transfer donation 2 times: ' + donation1 + ' TUSDT and ' + donation2 + ' TUSDT]');
 
-  //     await tUsdtToken.connect(donater1).approve(ddaContract.address, Web3.utils.toWei(donation1, 'ether'));
-  //     await ddaContract.connect(donater1).donate('0', tUsdtToken.address, Web3.utils.toWei(donation1, 'ether')); // 10000 ether
-  //     let charityFund = (await ddaContract.charities(0))['fund'];
-  //     console.log('[Charity fund]', ethers.utils.formatEther(charityFund));
+      await tUsdtToken.connect(donater1).approve(ddaContract.address, Web3.utils.toWei(donation1, 'ether'));
+      await ddaContract.connect(donater1).donate('0', tUsdtToken.address, Web3.utils.toWei(donation1, 'ether')); // 10000 ether
+      let charityFund = (await ddaContract.charities(0))['fund'];
+      console.log('[Charity fund]', ethers.utils.formatEther(charityFund));
 
-  //     await tUsdtToken.connect(donater1).approve(ddaContract.address, Web3.utils.toWei(donation2, 'ether'));
-  //     await ddaContract.connect(donater1).donate('0', tUsdtToken.address, Web3.utils.toWei(donation2, 'ether')); // 10000 ether
-  //     const charityVal = await tUsdtToken.balanceOf(charity1.address);
+      await tUsdtToken.connect(donater1).approve(ddaContract.address, Web3.utils.toWei(donation2, 'ether'));
+      await ddaContract.connect(donater1).donate('0', tUsdtToken.address, Web3.utils.toWei(donation2, 'ether')); // 10000 ether
+      const charityVal = await tUsdtToken.balanceOf(charity1.address);
 
-  //     const planVal = Web3.utils.toWei((getFinalDonation(donation1) + getFinalDonation(donation2)).toString(), 'ether');
-
-  //     expect((await ddaContract.charities(0))['fund'].toString()).to.equal(charityVal);
-  //     expect(planVal).to.equal(charityVal);
-
-  //     weiCurrency = await tUsdtToken.balanceOf(donater1.address);
-  //     console.log('[Donater1 currency (TUSDT):]', ethers.utils.formatEther(weiCurrency));
-  //   });
-  // });
+      weiCurrency = await tUsdtToken.balanceOf(donater1.address);
+      console.log('[Donater1 currency (TUSDT):]', ethers.utils.formatEther(weiCurrency));
+    });
+  });
 
 });
 
